93054d3f1ceefb85201fda775634f3f5e662e0c9
Erik Pohl <epohl@dmigs.com>
2024-11-21 22:15:21 -0500

    flake8 linted main.py and made necessary changes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import webbrowser
 import pandas as pd
+import random
 
 
 # output dictionary as spreadsheet
@@ -16,29 +17,29 @@ def read_from_bookmarks(bookmark_file):
                     url_start_loc = line.find('A HREF="') + 8
                     url_end_loc = line.find('"', url_start_loc + 1)
                     url = line[url_start_loc:url_end_loc]
-                    name_start_loc = line.find('">', url_end_loc) + 2       
+                    name_start_loc = line.find('">', url_end_loc) + 2
                     name_end_loc = line.find('</A>', name_start_loc)
                     name = line[name_start_loc:name_end_loc]
                     bookmarks_names_urls.update({name: url})
     return bookmarks_names_urls
-         
+
+
 def write_to_csv(song_list):
 
-    (pd.DataFrame.from_dict(    data=song_list, orient='index')
+    (pd.DataFrame.from_dict(data=song_list, orient='index')
      .to_csv('dict_file.csv', header=False))
+
 
 def read_from_csv(csv_file_name):
     pass
 
+
 if __name__ == '__main__':
     playlist = {}
-
     playlist = read_from_bookmarks('bookmarks_11_13_23.html')
     print(playlist)
     write_to_csv(playlist)
-
-    import random
     name, url = random.choice(list(playlist.items()))
     print(f"opening {name} using {url}")
     chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
-    webbrowser.get(chrome_path).open(url,2)
+    webbrowser.get(chrome_path).open(url, 2)
