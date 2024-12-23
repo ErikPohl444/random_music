3d0c2b181236b5ee104ae5fbdb5767723b40430a
Erik Pohl <epohl@dmigs.com>
2024-12-22 22:47:02 -0500

    added logging and refactored variable names

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import webbrowser
 import pandas as pd
 import random
 import json
+from setup_logging import logger
 
 
 def read_from_bookmarks(bookmark_file):
@@ -17,15 +18,19 @@ def read_from_bookmarks(bookmark_file):
                     name_end_loc = line.find('</A>', name_start_loc)
                     name = line[name_start_loc:name_end_loc]
                     bookmarks_names_urls.update({name: url})
-    print(bookmarks_names_urls)
-    prepared_dict = {i: x for i, x in enumerate(bookmarks_names_urls.items())}
-
+    prepared_dict = {
+        song_key: song_value
+        for song_key, song_value
+        in enumerate(
+            bookmarks_names_urls.items()
+        )
+    }
     bookmarks_names_urls = pd.DataFrame.from_dict(
         prepared_dict,
         orient="index",
         columns=['Song_Name', 'Song_URL']
     )
-    print(bookmarks_names_urls)
+    logger.info(f"loaded {len(bookmarks_names_urls)} songs into a song list")
     return bookmarks_names_urls
 
 
@@ -35,10 +40,16 @@ def write_to_csv(csv_file_name, song_list):
         columns=['Song_Name', 'Song_URL'],
         index_label="Index"
     )
+    logger.info(
+        f"completed writing {len(song_list)} songs "
+        f"from song list into {csv_file_name}"
+    )
+    return True
 
 
 def read_from_excel(excel_file_name):
-    songs = pd.read_excel('test.xlsx', usecols=["Song_Name", "Song_URL"])
+    songs = pd.read_excel(excel_file_name, usecols=["Song_Name", "Song_URL"])
+    logger.info(f"loaded {len(songs)} songs into the song list")
     return songs
 
 
@@ -48,27 +59,26 @@ def read_from_csv(csv_file_name):
         header=0,
         names=['Song_Name', 'Song_URL']
     )
-    print("song list")
-    print(song_list)
-    print("end song list")
+    logger.info(f"loaded {len(song_list)} songs into the song list")
     return song_list
 
 
 if __name__ == '__main__':
     with open("config.json") as config_handle:
         configs = json.load(config_handle)
+    logger.info("loaded program configurations")
     # save this for later!
-    songs = read_from_bookmarks(configs["bookmarks"])
-    # songs = read_from_excel('test.xlsx')
-    print(songs)
-    csv_file_name = configs["csv_file_name"]
+    # songs = read_from_bookmarks(configs["bookmarks"])
+    songs = read_from_excel(configs["xlsx_file_name"])
+    # print(songs)
+    # csv_file_name = configs["csv_file_name"]
     # songs = read_from_csv(csv_file_name)
-    songs.to_excel("test.xlsx", index_label="Index")
+    # songs.to_excel(configs["xlsx_file_name"], index_label="Index")
     # song_list = dict(songs.values.tolist())
     # save this for later
     # write_to_csv(csv_file_name, songs)
     (songlist_item_name,
      songlist_item_url) = random.choice(songs.values.tolist())
-    print(f"opening {songlist_item_name} using {songlist_item_url}")
+    logger.info(f"opening {songlist_item_name} using {songlist_item_url}")
     chrome_path = r'open -a /Applications/Google\ Chrome.app %s'
     webbrowser.get(chrome_path).open(songlist_item_url, 2)
