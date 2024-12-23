3ad0a94e39dad8f60fb0eb6366af8c70f35f139f
Erik Pohl <epohl@dmigs.com>
2024-12-22 20:22:51 -0500

    resolved read from bookmarks into dataframe and write to csv and also to excel

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ import pandas as pd
 import random
 import json
 
-# output dictionary as spreadsheet
-# class entries in spreadsheet by category
-# select only entries in a category
-# angular front end?
-
 
 def read_from_bookmarks(bookmark_file):
     bookmarks_names_urls = {}
@@ -22,16 +17,32 @@ def read_from_bookmarks(bookmark_file):
                     name_end_loc = line.find('</A>', name_start_loc)
                     name = line[name_start_loc:name_end_loc]
                     bookmarks_names_urls.update({name: url})
+    print(bookmarks_names_urls)
+    prepared_dict = {i: x for i, x in enumerate(bookmarks_names_urls.items())}
+
+    bookmarks_names_urls = pd.DataFrame.from_dict(
+        prepared_dict,
+        orient="index",
+        columns=['Song_Name', 'Song_URL']
+    )
+    print(bookmarks_names_urls)
     return bookmarks_names_urls
 
 
-def write_to_csv(song_list):
-    (pd.DataFrame.from_dict(data=song_list, orient='index')
-     .to_csv('dict_file.csv', header=False))
+def write_to_csv(csv_file_name, song_list):
+    song_list.to_csv(
+        csv_file_name,
+        columns=['Song_Name', 'Song_URL'],
+        index_label="Index"
+    )
 
 
 def read_from_csv(csv_file_name):
-    song_list = pd.read_csv(csv_file_name, header=None)
+    song_list = pd.read_csv(
+        csv_file_name,
+        header=0,
+        names=['Song_Name', 'Song_URL']
+    )
     return song_list
 
 
@@ -39,14 +50,16 @@ if __name__ == '__main__':
     with open("config.json") as config_handle:
         configs = json.load(config_handle)
     # save this for later!
-    # playlist = read_from_bookmarks(configs["bookmarks"])
+    songs = read_from_bookmarks(configs["bookmarks"])
+    print(songs)
     csv_file_name = configs["csv_file_name"]
-    songs = read_from_csv(csv_file_name)
-    song_list = dict(songs.values.tolist())
+    # songs = read_from_csv(csv_file_name)
+    songs.to_excel("test.xlsx", index_label="Index")
+    # song_list = dict(songs.values.tolist())
     # save this for later
-    # write_to_csv(csv_file_name, song_list)
+    # write_to_csv(csv_file_name, songs)
     (songlist_item_name,
-     songlist_item_url) = random.choice(list(song_list.items()))
+     songlist_item_url) = random.choice(songs.values.tolist())
     print(f"opening {songlist_item_name} using {songlist_item_url}")
     chrome_path = r'open -a /Applications/Google\ Chrome.app %s'
     webbrowser.get(chrome_path).open(songlist_item_url, 2)
